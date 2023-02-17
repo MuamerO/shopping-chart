3c8c7fe7d6dd940181ff389e35ac5301446af24b
Muamer <muamero@live.com>
2023-02-17 17:10:16 +0100

    add items to fall winter spring

--- src/Pages/ProductLine/Fall/Fall.jsx
+++ src/Pages/ProductLine/Fall/Fall.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import Filter from "Components/filter/Filter";
+import "../ProductLine.css";
+import "./fall.css";
+import { ShopItemCard } from "Components";
 
 const Fall = () => {
-  return <div>fall</div>;
+  return (
+    <div className="productLineShopContainer">
+      <Filter />
+      <div className="productLineItemCards">
+        <ShopItemCard
+          image="https://images.unsplash.com/photo-1595777457583-95e059d581b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1383&q=80"
+          title="title"
+          subtitle="subtitle"
+          price="22"
+        />
+        <ShopItemCard
+          image="https://images.unsplash.com/photo-1595777457583-95e059d581b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1383&q=80"
+          title="title"
+          subtitle="subtitle"
+          price="22"
+        />{" "}
+        <ShopItemCard
+          image="https://images.unsplash.com/photo-1595777457583-95e059d581b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1383&q=80"
+          title="title"
+          subtitle="subtitle"
+          price="22"
+        />{" "}
+        <ShopItemCard
+          image="https://images.unsplash.com/photo-1595777457583-95e059d581b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1383&q=80"
+          title="title"
+          subtitle="subtitle"
+          price="22"
+        />{" "}
+        <ShopItemCard
+          image="https://images.unsplash.com/photo-1595777457583-95e059d581b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1383&q=80"
+          title="title"
+          subtitle="subtitle"
+          price="22"
+        />{" "}
+        <ShopItemCard
+          image="https://images.unsplash.com/photo-1595777457583-95e059d581b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1383&q=80"
+          title="title"
+          subtitle="subtitle"
+          price="22"
+        />{" "}
+        <ShopItemCard
+          image="https://images.unsplash.com/photo-1595777457583-95e059d581b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1383&q=80"
+          title="title"
+          subtitle="subtitle"
+          price="22"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Fall;
--- src/Pages/ProductLine/Winter/Winter.jsx
+++ src/Pages/ProductLine/Winter/Winter.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import Filter from "Components/filter/Filter";
+import "../ProductLine.css";
+import "./winter.css";
+import { ShopItemCard } from "Components";
 
 const Winter = () => {
-  return <div>Winter Page</div>;
+  return (
+    <div className="productLineShopContainer">
+      <Filter />
+      <div className="productLineItemCards">
+        <ShopItemCard
+          image="https://images.unsplash.com/photo-1506682025334-be4f5a5ac8a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1988&q=80"
+          title="title"
+          subtitle="subtitle"
+          price="22"
+        />
+        <ShopItemCard
+          image="https://images.unsplash.com/photo-1506682025334-be4f5a5ac8a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1988&q=80"
+          title="title"
+          subtitle="subtitle"
+          price="22"
+        />{" "}
+        <ShopItemCard
+          image="https://images.unsplash.com/photo-1506682025334-be4f5a5ac8a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1988&q=80"
+          title="title"
+          subtitle="subtitle"
+          price="22"
+        />{" "}
+        <ShopItemCard
+          image="https://images.unsplash.com/photo-1506682025334-be4f5a5ac8a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1988&q=80"
+          title="title"
+          subtitle="subtitle"
+          price="22"
+        />{" "}
+        <ShopItemCard
+          image="https://images.unsplash.com/photo-1506682025334-be4f5a5ac8a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1988&q=80"
+          title="title"
+          subtitle="subtitle"
+          price="22"
+        />{" "}
+        <ShopItemCard
+          image="https://images.unsplash.com/photo-1506682025334-be4f5a5ac8a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1988&q=80"
+          title="title"
+          subtitle="subtitle"
+          price="22"
+        />{" "}
+        <ShopItemCard
+          image="https://images.unsplash.com/photo-1506682025334-be4f5a5ac8a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1988&q=80"
+          title="title"
+          subtitle="subtitle"
+          price="22"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Winter;
