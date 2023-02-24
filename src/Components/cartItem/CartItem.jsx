51a463fc9a7d8d76cb6cf60ec2f197a752927bb0
Muamer <muamero@live.com>
2023-02-24 11:43:49 +0100

    Cart CSS changes

--- src/Components/cartItem/CartItem.jsx
+++ src/Components/cartItem/CartItem.jsx
@@ -4,11 +4,21 @@ import { useDispatch, useSelector } from "react-redux";
 const CartItem = ({ id, price, img, amount, size, title }) => {
   return (
     <div className="container">
-      <img className="image" src={img} alt={title} />
-      <div className="attriutes">
-        <p className="price">{`price: ${price}`}</p>
-        <p className="size">{`size: ${size}`}</p>
-        <p className="amount">{`amount: ${amount}`}</p>
+      <div className="attributes">
+        <img className="image" src={img} alt={title} />
+        <div className="textCart">
+          <p className="cartTitle">{`Product: ${title}`}</p>
+          <p className="cartSize">{`Size: ${size}`}</p>
+          <p className="cartPrice">{`Price: ${price}`}</p>
+        </div>
+      </div>
+      <div className="cartItemAdding">
+        <div className="addMoreItems">
+          <p className="incDec">+</p>
+          <p className="amount">{`${amount}`}</p>
+          <p className="incDec">-</p>
+        </div>
+        <p className="itemSubtotas">{`Total: ${price * amount}`}</p>
       </div>
     </div>
   );
--- src/Pages/ShoppingCart/ShoppingCart.jsx
+++ src/Pages/ShoppingCart/ShoppingCart.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import "../ShoppingCart/shoppingCart.css";
 import { Link } from "react-router-dom";
 import CartItem from "Components/cartItem/CartItem";
 import { useDispatch, useSelector } from "react-redux";
+import { NavBar } from "Components";
 
 const ShoppingCart = () => {
   const item = useSelector((state) => state.cart.cartItems);
@@ -10,11 +11,13 @@ const ShoppingCart = () => {
     return <CartItem key={item.id} {...item} />;
   });
 
+  const noCartItems = <div> No cart items </div>;
+
   return (
-    <div>
-      <Link to={"/"}>Home</Link>
-      <hr />
-      {inCartItems}
+    <div className="cartContainer">
+      <NavBar />
+      {item.length == 0 ? noCartItems : inCartItems}
+      <div className="grandTotal">Total: Sum off all items</div>
     </div>
   );
 };
