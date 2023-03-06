63922afd09dec80d236338a297044cd3b9a9248c
Muamer <muamero@live.com>
2023-03-06 20:52:49 +0100

    fix data

--- src/itemCardData.js
+++ src/itemCardData.js
@@ -11,8 +11,8 @@ export default [
     id: 2,
     image:
       "https://images.unsplash.com/photo-1599309329365-0a9ed45a1da3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
-    title: `Summer collection dsaaaaaaaaaaaaadsadsadasds`,
-    subtitle: `Check out new summer collection     sadasdasdas             asdsa`,
+    title: `Summer collection`,
+    subtitle: `Check out new summer collection`,
     price: 80,
   },
   {
